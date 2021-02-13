89ade1e32d62fba187a4c309984db503368da732
Anthony Lim <anthony.hong.cheol.lim@gmail.com>
2021-02-13 15:03:46 -0600

    Initial Commit

---
+++ Shared/ErrorAssessment_Bessel_Function_Anthony_limApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorAssessment_Bessel_Function_Anthony_limApp.swift
+//  Shared
+//
+//  Created by Anthony Lim on 2/13/21.
+//
+
+import SwiftUI
+
+@main
+struct ErrorAssessment_Bessel_Function_Anthony_limApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
